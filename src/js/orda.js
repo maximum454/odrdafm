2c97c3f2b475179e244c54c4c11f101609985eb8
maxim <maximum454@gmail.com>
2018-09-07 20:20:38 +0600

    проект

--- src/js/orda.js
+++ src/js/orda.js
@@ -4,5 +4,17 @@ $(function () {
     $("a[href='#']").click(function () {
         return false;
     });
+    menu();
 });
+
+function menu() {
+    var menuWidth = $('.menu').innerWidth();
+    console.log(menuWidth);
+    $('.js-menu').on('click', function () {
+        $('.menu,.wrp').toggleClass('menu-active');
+    });
+    $('.js-close').on('click', function () {
+        $('.menu,.wrp').removeClass('menu-active');
+    });
+}
 
